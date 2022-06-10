433e6105fe04f8fffd8d24bbac78546809da72e3
Lucas Ribolli <lucasribolli@gmail.com>
2022-06-10 08:35:56 -0300

    add StaticUtilsTest

---
+++ Testing/android-testing-todo/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,36 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        val tasks = listOf(
+            Task("title", "desc", isCompleted = false)
+        )
+        val result = getActiveAndCompletedStats(tasks)
+        // "is" is a reserved keyword that is why we use "`" to differentiate as the Hamcrest "is"
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(100f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_null_returnsZeros() {
+        val result = getActiveAndCompletedStats(null)
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnsZeros() {
+        val tasks = emptyList<Task>()
+        val result = getActiveAndCompletedStats(tasks)
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+
+}
